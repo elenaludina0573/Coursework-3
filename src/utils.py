faad04f239b1faca69f8ea3b9b87812137984e88
elenaludina0573 <elenaludina0@gmail.com>
2023-12-26 23:12:16 +0300

    get_sort_operations

--- src/utils.py
+++ src/utils.py
@@ -1,10 +1,27 @@
 import json
-from config import DATA_DIR
+import datetime
+
 
 def get_all_operations(path):
     with open(path) as file:
         content = json.load(file)
         return content
+
+operation_list = []
+def get_executed_only():
+    if {}.get('state') == 'EXECUTED':
+        operation_list.append('state')
+        return operation_list
+def get_sort_operations(operation_list):
+    get_sort_operations = sorted(operation_list, key=lambda x: x['data'], reverse=True)
+    return get_sort_operations
+
+
+
+
+
+
+
 
 
 
