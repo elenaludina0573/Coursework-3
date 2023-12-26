87527ccae784ab41b57bc3ffa77c2b616312f725
elenaludina0573 <elenaludina0@gmail.com>
2023-12-27 01:02:10 +0300

    The path to the file

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 import os.path
 
 ROOT_DIR = os.path.dirname(__file__)
-DATA_DIR = os.path.join(ROOT_DIR, 'src', 'operations.json')
+OPERATION_FAIL_PATH = os.path.join(ROOT_DIR, 'src', 'operations.json')
