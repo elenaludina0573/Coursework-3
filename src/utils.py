a5f661d81fe552677d1c9b7bdb5d631090e664d2
elenaludina0573 <elenaludina0@gmail.com>
2023-12-26 23:32:26 +0300

    get_from_date

--- src/utils.py
+++ src/utils.py
@@ -12,9 +12,18 @@ def get_executed_only():
     if {}.get('state') == 'EXECUTED':
         operation_list.append('state')
         return operation_list
-def get_sort_operations(operation_list):
+def get_sort_operations():
     get_sort_operations = sorted(operation_list, key=lambda x: x['data'], reverse=True)
     return get_sort_operations
+
+""""
+    Функция преобразует дату 
+"""
+
+def get_from_date ():
+    form_date = datetime.datetime.fromisoformat(operation_list['date']).strftime('%d%m%y')
+    return form_date
+
 
 
 
