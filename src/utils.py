482b401cc09086e548c0026c304d0f47b54c6b41
elenaludina0573 <elenaludina0@gmail.com>
2023-12-26 23:45:09 +0300

    hide_requisites

--- src/utils.py
+++ src/utils.py
@@ -16,13 +16,26 @@ def get_sort_operations():
     get_sort_operations = sorted(operation_list, key=lambda x: x['data'], reverse=True)
     return get_sort_operations
 
-""""
+"""
     Функция преобразует дату 
 """
 
 def get_from_date ():
     form_date = datetime.datetime.fromisoformat(operation_list['date']).strftime('%d%m%y')
     return form_date
+
+"""
+   Функция кодирует карту и счет
+"""
+def hide_requisites(requistes:str):
+	parts = requistes.split()
+	namber = parts[-1]
+	if reguistes.lower().statwith('счет'):
+		hided_namber = f"**{number[-4:]}"
+	else:
+		hided_namber = f"{number[:4]} {number[4:6]} ** **** {number[-4:]}"
+	return ' '.join(parts)
+
 
 
 
