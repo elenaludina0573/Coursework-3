9be4f9ecdf95aa6af5b5c4662f9b0269876df88a
elenaludina0573 <elenaludina0@gmail.com>
2023-12-27 21:13:41 +0300

    Utils c errors

--- src/utils.py
+++ src/utils.py
@@ -1,62 +1,71 @@
 import json
-import datetime
+
 
 """"
     Функция берет данные из json
 """
 def get_all_operations(path):
-    with open(path) as file:
+    with open(path, "r", encoding="UTF-8") as file:
         content = json.load(file)
         return content
 """
     Создаем новый список, убираем все лишнее
 """
 operation_list = []
-def get_executed_only():
+def get_executed_only(operation:list):
     if {}.get('state') == 'EXECUTED':
         operation_list.append('state')
         return operation_list
+
 """
     Сортируем список по датам
 """
-def get_sort_operations():
-    get_sort_operations = sorted(operation_list, key=lambda x: x['data'], reverse=True)
-    return get_sort_operations
+sort_operation_list =[]
+def get_sort_operations(operation:list):
+    get_sort_operations = operation_list.sort(key=lambda x: x.get["date"], reverse=True)
+    sort_operation_list.append('date')
+    return sort_operation_list
 
 """
     Функция преобразует дату в нужный формат
 """
-def get_from_date ():
-    form_date = datetime.datetime.fromisoformat(operation_list['date']).strftime('%d%m%y')
+def get_from_date (operation:list):
+    form_date = int()
+    for date in sort_operation_list:
+        date_1 = date.split('T')
+        date_2 = date_1[0].split('-')
+        form_date = '.'.join(reversed(date_2))
     return form_date
 
 """
    Функция кодирует карту и счет
 """
 def hide_requisites(requistes:str):
-	parts = requistes.split()
-	namber = parts[-1]
-	if reguistes.lower().statwith('счет'):
-		hided_namber = f"**{number[-4:]}"
-	else:
-		hided_namber = f"{number[:4]} {number[4:6]} ** **** {number[-4:]}"
-	return ' '.join(parts)
+    parts = requistes.split()
+    number = parts[-1]
+    if parts.lower().statwith('счет'):
+        hided_number = f"**{number[-4:]}"
+    else:
+        hided_number = f"{number[:4]} {number[4:6]} ** **** {number[-4:]}"
+        parts[-1] = hided_number
+    return ' '.join(parts)
 
 """
     Функция возвращает всю информацию по операции
 """
-def get_formated_operations(operations_list):
-    formated_date = formate_data(operation_list['date'])
-    type_operation = operation_list['description']
-    line_one_output = f"{formated_date} {type_operation}"
-    if operations_list.get('from'):
-        hided_from = hide_requisites(operations_list.get('from'))
+def get_formated_operations(operation):
+    from_date = get_from_date(operation)
+    type_operation = operation['description']
+    line_one_output = f"{from_date} {type_operation}"
+    if operation.get('from'):
+        hided_from = hide_requisites(operation.get('from'))
     else:
         hided_from = 'Нет данных'
-    hided_to = hide_requisites(operations_list.get('to'))
+    hided_to = hide_requisites(operation.get('to'))
     line_two_output = f"{hided_from} -->  {hided_to}"
-    amount = operations_list['operationAmount']['amount']
-    currency = operations_list['operationAmount']['currency']['name']
+
+    amount = operation['operationAmount']['amount']
+    currency = operation['operationAmount']['currency']['name']
     line_three_output = f"{amount} {currency}"
     return (f"{line_one_output}"
             f"{line_two_output}"
