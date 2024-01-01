786bba10fc1643de4d08de30ee0ff6bd0f0a8539
elenaludina0573 <elenaludina0@gmail.com>
2024-01-01 22:23:48 +0300

    The final result

--- src/main.py
+++ src/main.py
@@ -1,4 +1,4 @@
-import os
+import os.path
 from src.utils import get_all_operations,get_executed_only,get_sort_operations,get_formated_operations
 from config import ROOT_DIR
 OPERATION_FAIL_PATH = os.path.join(ROOT_DIR, 'src', 'operations.json')
@@ -7,8 +7,8 @@ OPERATION_FAIL_PATH = os.path.join(ROOT_DIR, 'src', 'operations.json')
 def main():
     all_operations = get_all_operations(OPERATION_FAIL_PATH)
     filtered_operations = get_executed_only(all_operations)
-    sorted_operations = get_sort_operations(filtered_operations)
-    for operation in sorted_operations[:5]:
+    sort_operations = get_sort_operations(filtered_operations)
+    for operation in sort_operations[:5]:
         print(get_formated_operations(operation))
 
 if __name__ == "__main__":
